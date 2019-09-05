a157e8b123ce22a40ac177b5cffe898b5083a155
Anthony Nguyen <avn253@gmail.com>
2019-09-05 13:29:34 -0500

    add files for ticker

---
+++ ticker/main.js
@@ -0,0 +1,29 @@
+updateTicker();
+
+function updateTicker() {
+    let tickerElement = document.getElementById("mainTicker");
+    
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://tpltest.cf/ticker/rawticker.txt', true);
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        var data = JSON.parse(request.responseText);
+        console.log(data);
+      } else {
+        // We reached our target server, but it returned an error
+        console.log(request);
+      }
+    };
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+      console.log(request);
+    };
+
+    request.send();
+
+
+}
